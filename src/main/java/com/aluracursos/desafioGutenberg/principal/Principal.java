f83127c5ef1032cce6419624f8f1503fb5db46ab
Veronica <vgalbicka@gmail.com>
2024-05-01 20:24:17 -0300

    Top 10
    Utilizando sorted. Comparator.comparin y en reversa porque originalmente los ordenaría de menor a mayor. También limitar a 10 e imprime

--- src/main/java/com/aluracursos/desafioGutenberg/principal/Principal.java
+++ src/main/java/com/aluracursos/desafioGutenberg/principal/Principal.java
@@ -1,8 +1,11 @@
 package com.aluracursos.desafioGutenberg.principal;
 
 import com.aluracursos.desafioGutenberg.model.Datos;
+import com.aluracursos.desafioGutenberg.model.DatosLibro;
 import com.aluracursos.desafioGutenberg.service.ConsumoAPI;
 import com.aluracursos.desafioGutenberg.service.ConvierteDatos;
+
+import java.util.Comparator;
 
 public class Principal {
     private static final String URL_BASE = "https://gutendex.com/books/";
@@ -13,5 +16,13 @@ public class Principal {
         System.out.println(json);
         var datos = conversor.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        //top 10 libros más descargados
+        System.out.println("top 10 libros más descargados ");
+        datos.resultados().stream()
+                .sorted(Comparator.comparing(DatosLibro::numeroDeDescargas).reversed())
+                .limit(10)
+                .forEach(System.out::println);
+
     }
 }
